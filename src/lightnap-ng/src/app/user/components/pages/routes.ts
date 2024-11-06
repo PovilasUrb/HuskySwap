b9be3e88f96e0fe6f43bc63fd86c1384b8683343
Hai Dang Nguyen <haidangn@uw.edu>
2024-11-05 19:03:17 -0800

    Created my-trades page

--- src/lightnap-ng/src/app/user/components/pages/routes.ts
+++ src/lightnap-ng/src/app/user/components/pages/routes.ts
@@ -2,4 +2,5 @@ import { AppRoute } from "@routing";
 
 export const Routes: AppRoute[] = [
   { path: "", data: { alias: "user-home" }, loadComponent: () => import("./index/index.component").then(m => m.IndexComponent) },
+  { path: "my-trades", data: { alias: "my-trades" }, loadComponent: () => import("./my-trades/my-trades.component").then(m => m.IndexComponent) },
 ];
