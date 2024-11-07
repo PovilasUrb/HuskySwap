f071807adcf193f111a82180b84529c26ec5c016
JDKaim <jdkaim@hotmail.com>
2024-11-07 13:54:22 -0800

    Fix core warnings for nullable strings

--- src/LightNap.Core/ClassInfos/Dto/Request/CreateClassInfoDto.cs
+++ src/LightNap.Core/ClassInfos/Dto/Request/CreateClassInfoDto.cs
@@ -3,10 +3,10 @@ namespace LightNap.Core.ClassInfos.Request.Dto
     public class CreateClassInfoDto
     {
         // TODO: Update which fields to include when creating this item.
-        public string Title { get; set; }
-		public string Description { get; set; }
-		public string Instructor { get; set; }
-		public string ClassCode { get; set; }
-		public string Notes { get; set; }
+        public required string Title { get; set; }
+		public required string Description { get; set; }
+		public required string Instructor { get; set; }
+		public required string ClassCode { get; set; }
+		public required string Notes { get; set; }
     }
 }
