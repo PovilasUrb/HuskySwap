1fbf2d782d63191a1bdf60197320c061e825ad4c
JD Kaim <36973058+JDKaim@users.noreply.github.com>
2024-11-07 13:43:27 -0800

    Update ClassInfoDto.cs

--- src/LightNap.Core/ClassInfos/Dto/Response/ClassInfoDto.cs
+++ src/LightNap.Core/ClassInfos/Dto/Response/ClassInfoDto.cs
@@ -4,10 +4,10 @@ namespace LightNap.Core.ClassInfos.Response.Dto
     {
         // TODO: Finalize which fields to include when returning this item.
         public int Id { get; set; }
-        public string Title { get; set; }
-		public string Description { get; set; }
-		public string Instructor { get; set; }
-		public string ClassCode { get; set; }
-		public string Notes { get; set; }
+        public required string Title { get; set; }
+	public required string Description { get; set; }
+	public required string Instructor { get; set; }
+	public required string ClassCode { get; set; }
+	public required string Notes { get; set; }
     }
 }
