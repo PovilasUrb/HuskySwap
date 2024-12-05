7b63985861af844cba7d7ab815d74b350495d797
Jacob <jacobc35@cs.washington.edu>
2024-12-04 23:10:45 -0800

    added home info page

--- src/lightnap-ng/src/app/user/components/pages/index/index.component.ts
+++ src/lightnap-ng/src/app/user/components/pages/index/index.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { CardModule } from 'primeng/card';
-
+import { ButtonModule } from 'primeng/button';
+import { RouterLink } from "@angular/router";
+import { RouteAliasService } from "@routing";
 @Component({
     selector: 'app-home-index',
     standalone: true,
     templateUrl: './index.component.html',
-    imports: [CardModule]
+    imports: [CardModule, ButtonModule, RouterLink]
 })
 export class IndexComponent {
+    items = [
+        { label: "All Classes", icon: "pi pi-fw pi-book", routerLink: this.routeAliasService.getRoute("class-infos") },
+        { label: "My Classes", icon: "pi pi-fw pi-book", routerLink: this.routeAliasService.getRoute("my-classes") },
+        { label: "My Wishlist", icon: "pi pi-fw pi-gift", routerLink: this.routeAliasService.getRoute("wishlist") },
+        { label: "Incoming Swaps", icon: "pi pi-fw pi-search", routerLink: this.routeAliasService.getRoute("incoming-swaps") },
+        { label: "Outgoing Swaps", icon: "pi pi-fw pi-sync", routerLink: this.routeAliasService.getRoute("outgoing-swaps") },
+        { label: "Completed Swaps", icon: "pi pi-fw pi-verified", routerLink: this.routeAliasService.getRoute("completed-swaps") }
+      ];
+      constructor(private routeAliasService: RouteAliasService) {}
 }
